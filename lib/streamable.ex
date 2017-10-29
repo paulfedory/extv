54d1037d17560c66f4da487307e20c689e3179b5
Andrew Pett <astuart.pett@gmail.com>
2017-10-29 09:40:11 -0600

    document streamable module

--- lib/streamable.ex
+++ lib/streamable.ex
@@ -1,4 +1,23 @@
 defmodule ExTV.Streamable do
+  @doc """
+  Sets up a stream for a paginated API call based on 3 functions;
+
+  - A function which given a page number, will call the API for the page
+  - A function which will extract the desired data from the response
+  - A function which will calculate the number of the next page from the current page and response
+
+  This module provides defaults for the last two functions: default_extract/1 and default_next_page/2
+  Errors raised when fetching a page will result in an empty enumeration.
+
+  Example:
+
+      iex> ExTV.Streamable.stream(
+      ...> fn page -> ExTV.HTTP.get("https://someapi.com/?page=#\{page\}") end,
+      ...> &ExTV.Streamable.default_extract/1,
+      ...> &ExTV.Streamable.default_next_page/2
+      ...> )
+      #Function<50.40091930/2 in Stream.resource/3>
+  """
   @spec stream((integer -> map()), (map() -> map()), (integer, map() -> integer)) :: Enumerable.t
   def stream(fetch_page, extract_data \\ &default_extract/1, calc_next_page \\ &default_next_page/2) do
     start_fn = fn ->
@@ -16,11 +35,30 @@ defmodule ExTV.Streamable do
     Stream.resource(start_fn, next_fn, stop_fn)
   end
 
+  @doc """
+  A default extraction function which will return the top level 'data' key in the response body
+
+  Example:
+      iex> ExTV.Streamable.default_extract(%{ body: %{ "data" => "Cookies" } })
+      {:ok, "Cookies"}
+  """
   @spec default_extract(map()) :: {:ok, map()}
   def default_extract(response) do
     {:ok, response.body["data"]}
   end
 
+  @doc """
+  A default function which will calculate the next page number to fetch based on the current
+  number and API response using the top level 'links' key which provides pagination info.
+  Returns nil when the final page is reached.
+
+  Example:
+      iex(4)> ExTV.Streamable.default_next_page(1, %{ body: %{ "links" => %{ "next" => 1 } } })
+      {:ok, nil}
+
+      iex(5)> ExTV.Streamable.default_next_page(1, %{ body: %{ "links" => %{ "next" => 2 } } })
+      {:ok, 2}
+  """
   @spec default_next_page(number, map()) :: {:ok, number} | {:ok, nil}
   def default_next_page(current_page, response) do
     next = case response.body["links"]["next"] do
